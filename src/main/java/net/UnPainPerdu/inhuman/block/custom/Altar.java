abb64c59f853202bcab5077ff03be87543c05beb
Ethan Capouillez <ethan.capouillez@hotmail.be>
2023-07-19 16:57:30 +0200

    Fix de l'altar (zone de clicage)

---
+++ src/main/java/net/UnPainPerdu/inhuman/block/custom/Altar.java
@@ -0,0 +1,93 @@
+package net.UnPainPerdu.inhuman.block.custom;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.DoublePlantBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.DoorHingeSide;
+import net.minecraft.world.level.block.state.properties.DoubleBlockHalf;
+import net.minecraft.world.level.block.state.properties.EnumProperty;
+import net.minecraft.world.level.material.Fluids;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
+
+import javax.annotation.Nullable;
+
+public class Altar extends Block {
+
+    public static final EnumProperty<DoubleBlockHalf> HALF = BlockStateProperties.DOUBLE_BLOCK_HALF;
+    public Altar(Properties pProperties) {
+        super(pProperties);
+        this.registerDefaultState(this.stateDefinition.any().setValue(HALF, DoubleBlockHalf.LOWER));
+    }
+    private static final VoxelShape SHAPE = Block.box(0, 0, 0, 16, 16, 16);
+    //forme hit-box, début-fin x y z
+
+    //Applique la hit-box
+    @Override
+    public VoxelShape getShape(BlockState p_60555_, BlockGetter p_60556_, BlockPos p_60557_, CollisionContext p_60558_) {
+        return SHAPE;
+
+    }
+    //check si le dessus est libre
+    @Nullable
+    public BlockState getStateForPlacement(BlockPlaceContext pContext) {
+        BlockPos blockpos = pContext.getClickedPos();
+        Level level = pContext.getLevel();
+        if (blockpos.getY() < level.getMaxBuildHeight() - 1 && level.getBlockState(blockpos.above()).canBeReplaced(pContext)) {
+            return this.defaultBlockState().setValue(HALF, DoubleBlockHalf.LOWER);
+        } else {
+            return null;
+        }
+    }
+
+    // Pose le bloc du dessus
+    public void setPlacedBy(Level pLevel, BlockPos pPos, BlockState pState, LivingEntity pPlacer, ItemStack pStack) {
+        pLevel.setBlock(pPos.above(), pState.setValue(HALF, DoubleBlockHalf.UPPER), 3);
+    }
+
+    //?
+    public boolean canSurvive(BlockState pState, LevelReader pLevel, BlockPos pPos) {
+        BlockPos blockpos = pPos.below();
+        BlockState blockstate = pLevel.getBlockState(blockpos);
+        return pState.getValue(HALF) == DoubleBlockHalf.LOWER ? blockstate.isFaceSturdy(pLevel, blockpos, Direction.UP) : blockstate.is(this);
+    }
+    //créé un nouveau BlockState nommé HALF
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
+        pBuilder.add(HALF);
+    }
+
+    //quand le bloc est pété
+    public void playerWillDestroy(Level pLevel, BlockPos pPos, BlockState pState, Player pPlayer) {
+        if (!pLevel.isClientSide && pPlayer.isCreative()) {
+            preventCreativeDropFromBottomPart(pLevel, pPos, pState, pPlayer);
+        }
+
+        super.playerWillDestroy(pLevel, pPos, pState, pPlayer);
+    }
+
+    //Pète le bloc du dessous si dessus cassé
+    protected static void preventCreativeDropFromBottomPart(Level pLevel, BlockPos pPos, BlockState pState, Player pPlayer) {
+        DoubleBlockHalf doubleblockhalf = pState.getValue(HALF);
+        if (doubleblockhalf == DoubleBlockHalf.UPPER) {
+            BlockPos blockpos = pPos.below();
+            BlockState blockstate = pLevel.getBlockState(blockpos);
+            if (blockstate.is(pState.getBlock()) && blockstate.getValue(HALF) == DoubleBlockHalf.LOWER) {
+                BlockState blockstate1 = blockstate.getFluidState().is(Fluids.WATER) ? Blocks.WATER.defaultBlockState() : Blocks.AIR.defaultBlockState();
+                pLevel.setBlock(blockpos, blockstate1, 35);
+                pLevel.levelEvent(pPlayer, 2001, blockpos, Block.getId(blockstate));
+            }
+        }
+    }
+}
--- src/main/java/net/UnPainPerdu/inhuman/block/custom/DraconicAltar.java
+++ src/main/java/net/UnPainPerdu/inhuman/block/custom/DraconicAltar.java
@@ -1,22 +1,15 @@
 package net.UnPainPerdu.inhuman.block.custom;
 
-import net.minecraft.world.level.block.Block;
 
-
-public class DraconicAltar extends Block {
+public class DraconicAltar extends Altar {
     //Partie pur bloc
 
     public DraconicAltar(Properties properties) {
         super(properties);
     }
-    /*
-    private static final VoxelShape SHAPE =
-            Block.box(0, 0, 0, 16, 10, 16);
 
-    @Override
-    public VoxelShape getShape(BlockState p_60555_, BlockGetter p_60556_, BlockPos p_60557_, CollisionContext p_60558_) {
-        return SHAPE;
-    }
+
+    /*
     //Partie entité
     @Nullable
     @Override
